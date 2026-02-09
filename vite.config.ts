f44c11267f2a340018e36354c2a9f6c1dd35944e
Karen Liseth <klmendez@unimayor.edu.co>
2026-02-09 16:50:40 -0500

    deploy github pages docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   base: '/',
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true,
-  },
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, 'src'),
     },
+  },
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
   },
 })
